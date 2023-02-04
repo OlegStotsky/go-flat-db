30bfa2340a4a8e9d6c608fa3f773d34552bb52da
Oleg Stotsky <ostotsky@gmail.com>
2023-02-04 18:27:20 +0400

    add unordered index opt

---
+++ opts.go
@@ -0,0 +1,12 @@
+package GoFlatDB
+
+func WithUnorderedIndex[T any](fieldName string) FlatDBCollectionOption[T] {
+	return func(db *FlatDBCollection[T]) {
+		db.unorderedIndexes[fieldName] = &flatDBIndexUnorderedIndex{
+			ordered:   false,
+			fieldName: fieldName,
+
+			data: map[interface{}]string{},
+		}
+	}
+}
